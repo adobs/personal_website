acea75cb55aa35861b844834a76fbbdcfb7c057d
Hackbright Student <info@hackbrightacademy.com>
2015-12-06 20:30:38 -0800

    added os

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template, redirect, flash, session
 from jinja2 import StrictUndefined
 # from flask_debugtoolbar import DebugToolbarExtension
-
+import os
 
 app = Flask(__name__)
 
@@ -20,8 +20,7 @@ def home():
 
 
 if __name__ == "__main__":
-    app.debug = True
-    # connect_to_db(app)    
+    
+    PORT = int(os.environ.get("PORT", 5000))
 
-    # DebugToolbarExtension(app)
-    app.run()
+    app.run(debug=True, host="0.0.0.0", port=PORT)
